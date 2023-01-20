e64d4027613045b0725e405c9da6306dafdab8d9
Artem Leonov <a.leonov@dats.team>
2023-01-20 16:37:40 +0300

    init project

---
+++ src/main/java/com/demo/learnvuejs/LearnVueJsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.learnvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnVueJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnVueJsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/learnvuejs/LearnVueJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.learnvuejs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnVueJsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
